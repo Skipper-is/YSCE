efb7b43356a8eec3c3737d42e9d0d8d0f97f022e
Skipper <skipperthepilot@googlemail.com>
2023-11-07 20:27:04 +0000

    Update fsnetwork.cpp

--- src/core/fsnetwork.cpp
+++ src/core/fsnetwork.cpp
@@ -1394,6 +1394,7 @@ YSRESULT FsSocketServer::BroadcastAirplaneState(void)
 
 YSRESULT FsSocketServer::StartEndurance(int clientId,unsigned char dat[],unsigned packetLength){
 	this->commandQueue.push(FSNCC_SVR_STARTENDURANCEMODE_JET);
+	this->commandQueue.push(FSNCC_SVR_TERMINATEMISSION);
 	return YSOK;
 }
 
